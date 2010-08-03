971da9214e3ee2ff412cc656ca8b40ac5a01ffaa
Kan Fushihara <kan@mfac.jp>
2010-08-03 19:32:26 +0900

    initial commit

---
+++ lib/DBIx/Skinny/Object.pm
@@ -0,0 +1,32 @@
+package DBIx::Skinny::Object;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::Skinny::Object -
+
+=head1 SYNOPSIS
+
+  use DBIx::Skinny::Object;
+
+=head1 DESCRIPTION
+
+DBIx::Skinny::Object is
+
+=head1 AUTHOR
+
+Kan Fushihara E<lt>kan@mfac.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
